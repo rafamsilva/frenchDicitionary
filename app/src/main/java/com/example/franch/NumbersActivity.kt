773fbd2805e3eb5b507274019127a5d55f363bae
Rafael de Melo Silva <rafamsilva@live.com>
2019-11-21 17:27:16 -0300

    adjust numberActivity

--- app/src/main/java/com/example/franch/NumbersActivity.kt
+++ app/src/main/java/com/example/franch/NumbersActivity.kt
@@ -1,12 +1,25 @@
 package com.example.franch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class NumbersActivity : AppCompatActivity() {
+
+    private val portugueseNumbers = arrayListOf("Um", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez")
+    private val frenchNumbers = arrayListOf("Un", "Deux", "Trois", "Quatre", "Cinq", "Six", "Sept", "Huit", "Neuf", "Dix")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_numbers)
+        renderNumbers()
     }
+
+    private fun renderNumbers() {
+        val listView: ListView = findViewById(R.id.numberView)
+        listView.setAdapter(ArrayAdapter<String>(this, R.layout.list_item, portugueseNumbers))
+    }
+
 }
